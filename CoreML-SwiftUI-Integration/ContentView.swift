637573f3e0c43799886faaef57dc66770b0a0000
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-18 15:56:39 -0700

    Initial Commit

---
+++ CoreML-SwiftUI-Integration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreML-SwiftUI-Integration
+//
+//  Created by Mitya Kim on 7/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreML-SwiftUI-Integration/CoreML_SwiftUI_IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_SwiftUI_IntegrationApp.swift
+//  CoreML-SwiftUI-Integration
+//
+//  Created by Mitya Kim on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_SwiftUI_IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
